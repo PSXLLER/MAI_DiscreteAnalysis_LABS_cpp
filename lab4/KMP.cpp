c5eff700649fd4c1a9c73d9823cec14106c97c3e
Danil_Druzhinin <dandruzhinin2000@gmail.com>
2026-05-02 01:36:27 +0300

    Z-func fix

--- lab4/KMP.cpp
+++ lab4/KMP.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <deque>
+#include <algorithm>
 #include <cstdint>
 
 struct TPosition {
@@ -10,46 +11,66 @@ struct TPosition {
     int Word;
 };
 
+std::vector<uint32_t> ReadPattern() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream iss(line);
+    std::vector<uint32_t> pat;
+    uint32_t v;
+    while (iss >> v) {
+        pat.push_back(v);
+    }
+    return pat;
+}
+
+std::vector<int> BuildZFunction(const std::vector<uint32_t>& s) {
+    int n = static_cast<int>(s.size());
+    std::vector<int> z(n, 0);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++) {
+        if (i < r) {
+            z[i] = std::min(r - i, z[i - l]);
+        }
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if (i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
+
 std::vector<int> BuildPrefixFunction(const std::vector<uint32_t>& pat) {
     int m = static_cast<int>(pat.size());
+
+    std::vector<int> z = BuildZFunction(pat);
+
     std::vector<int> pf(m, 0);
     for (int i = 1; i < m; i++) {
-        int j = pf[i - 1];
-        while (j > 0 && pat[i] != pat[j]) {
-            j = pf[j - 1];
+        if (z[i] > 0) {
+            pf[i + z[i] - 1] = std::max(pf[i + z[i] - 1], z[i]);
         }
-        if (pat[i] == pat[j]) {
-            j++;
-        }
-        pf[i] = j;
     }
+    for (int i = m - 2; i >= 0; i--) {
+        if (pf[i + 1] > 0) {
+            pf[i] = std::max(pf[i], pf[i + 1] - 1);
+        }
+    }
+
+    z.clear();
+    z.shrink_to_fit();
+
     return pf;
 }
 
-int main() {
-    std::string line;
-    std::getline(std::cin, line);
-
-    std::vector<uint32_t> pat;
-    {
-        std::istringstream iss(line);
-        uint32_t v;
-        while (iss >> v) {
-            pat.push_back(v);
-        }
-    }
-
+void Search(const std::vector<uint32_t>& pat, const std::vector<int>& pf) {
     int m = static_cast<int>(pat.size());
-    if (m == 0) {
-        return 0;
-    }
-
-    std::vector<int> pf = BuildPrefixFunction(pat);
-
     std::deque<TPosition> win;
     int matched = 0;
     int textLine = 1;
-
+    std::string line;
     while (std::getline(std::cin, line)) {
         std::istringstream iss(line);
         uint32_t v;
@@ -73,6 +94,14 @@ int main() {
         }
         textLine++;
     }
+}
 
+int main() {
+    std::vector<uint32_t> pat = ReadPattern();
+    if (pat.empty()) {
+        return 0;
+    }
+    std::vector<int> pf = BuildPrefixFunction(pat);
+    Search(pat, pf);
     return 0;
 }
